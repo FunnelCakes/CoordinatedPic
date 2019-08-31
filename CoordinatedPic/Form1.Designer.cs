cf114677b491c304090f89e409a066d6cb876d31
FunnelCakes <41977746+FunnelCakes@users.noreply.github.com>
2019-08-31 12:39:18 +0800

    版本1.0
    ·很痛苦。爷没写图片缩放
    ·后续版本补上，nmd

---
+++ CoordinatedPic/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CoordinatedPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MouseClickedCoordinationX = new System.Windows.Forms.TextBox();
+            this.Locate = new System.Windows.Forms.Button();
+            this.XCoordination = new System.Windows.Forms.TrackBar();
+            this.YCoordination = new System.Windows.Forms.TrackBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.MouseOverCoordination = new System.Windows.Forms.Label();
+            this.IfClear = new System.Windows.Forms.CheckBox();
+            this.MouseClickedCoordinationY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XCoordination)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YCoordination)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 243);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // MouseClickedCoordinationX
+            // 
+            this.MouseClickedCoordinationX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MouseClickedCoordinationX.Location = new System.Drawing.Point(56, 316);
+            this.MouseClickedCoordinationX.Name = "MouseClickedCoordinationX";
+            this.MouseClickedCoordinationX.Size = new System.Drawing.Size(29, 21);
+            this.MouseClickedCoordinationX.TabIndex = 1;
+            // 
+            // Locate
+            // 
+            this.Locate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Locate.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Locate.Location = new System.Drawing.Point(162, 316);
+            this.Locate.Name = "Locate";
+            this.Locate.Size = new System.Drawing.Size(75, 23);
+            this.Locate.TabIndex = 3;
+            this.Locate.Text = "Locate";
+            this.Locate.UseVisualStyleBackColor = true;
+            this.Locate.Click += new System.EventHandler(this.Locate_Click);
+            // 
+            // XCoordination
+            // 
+            this.XCoordination.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.XCoordination.Location = new System.Drawing.Point(51, 250);
+            this.XCoordination.Maximum = 425;
+            this.XCoordination.Name = "XCoordination";
+            this.XCoordination.Size = new System.Drawing.Size(413, 45);
+            this.XCoordination.TabIndex = 5;
+            this.XCoordination.ValueChanged += new System.EventHandler(this.XCoordination_ValueChanged);
+            // 
+            // YCoordination
+            // 
+            this.YCoordination.Location = new System.Drawing.Point(4, 4);
+            this.YCoordination.Maximum = 241;
+            this.YCoordination.Name = "YCoordination";
+            this.YCoordination.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.YCoordination.Size = new System.Drawing.Size(45, 243);
+            this.YCoordination.TabIndex = 6;
+            this.YCoordination.ValueChanged += new System.EventHandler(this.YCoordination_ValueChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "(*.bmp)|*.bmp|(*.jpg)|*.jpg|(*.png)|*.png|(*.gif)|*.gif|(*.*)|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // MouseOverCoordination
+            // 
+            this.MouseOverCoordination.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MouseOverCoordination.AutoSize = true;
+            this.MouseOverCoordination.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MouseOverCoordination.Location = new System.Drawing.Point(54, 296);
+            this.MouseOverCoordination.Name = "MouseOverCoordination";
+            this.MouseOverCoordination.Size = new System.Drawing.Size(47, 14);
+            this.MouseOverCoordination.TabIndex = 7;
+            this.MouseOverCoordination.Text = "(0,0)";
+            // 
+            // IfClear
+            // 
+            this.IfClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.IfClear.AutoSize = true;
+            this.IfClear.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.IfClear.Location = new System.Drawing.Point(56, 343);
+            this.IfClear.Name = "IfClear";
+            this.IfClear.Size = new System.Drawing.Size(146, 18);
+            this.IfClear.TabIndex = 4;
+            this.IfClear.Text = "clean last mark";
+            this.IfClear.UseVisualStyleBackColor = true;
+            // 
+            // MouseClickedCoordinationY
+            // 
+            this.MouseClickedCoordinationY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MouseClickedCoordinationY.Location = new System.Drawing.Point(112, 316);
+            this.MouseClickedCoordinationY.Name = "MouseClickedCoordinationY";
+            this.MouseClickedCoordinationY.Size = new System.Drawing.Size(29, 21);
+            this.MouseClickedCoordinationY.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(91, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = ",";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(273, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 22);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Choose a Picture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Locate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 395);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MouseClickedCoordinationY);
+            this.Controls.Add(this.IfClear);
+            this.Controls.Add(this.MouseOverCoordination);
+            this.Controls.Add(this.YCoordination);
+            this.Controls.Add(this.XCoordination);
+            this.Controls.Add(this.Locate);
+            this.Controls.Add(this.MouseClickedCoordinationX);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XCoordination)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YCoordination)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox MouseClickedCoordinationX;
+        private System.Windows.Forms.Button Locate;
+        private System.Windows.Forms.TrackBar XCoordination;
+        private System.Windows.Forms.TrackBar YCoordination;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label MouseOverCoordination;
+        private System.Windows.Forms.CheckBox IfClear;
+        private System.Windows.Forms.TextBox MouseClickedCoordinationY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CoordinatedPic/Form1.cs
@@ -0,0 +1,156 @@
+﻿//Form1.cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoordinatedPic
+{
+    public partial class Form1 : Form
+    {
+            //Author:张凯翔
+            //copyright 2019
+            //allrightsreserved
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int x, y,x1,y1;//初始化鼠标坐标值
+        private bool Drawable=false;
+        private int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+        private int ScreenHeight=Screen.PrimaryScreen.Bounds.Height;//获取屏幕分辨率
+        private Graphics g;
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            g.Clear(pictureBox1.BackColor);
+            XCoordination.SetBounds(pictureBox1.Location.X, pictureBox1.Location.Y + pictureBox1.Size.Height + 2, pictureBox1.Width, 45);
+            YCoordination.SetBounds(pictureBox1.Location.X - 2 - 45, pictureBox1.Location.Y, 45, pictureBox1.Height);
+            XCoordination.Maximum = XCoordination.Width;
+            YCoordination.Maximum = YCoordination.Height;
+        }//设置UI自适应缩放
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Drawable == false)
+            {
+                x = e.X; y = pictureBox1.Height - e.Y;
+
+                MouseOverCoordination.Text = x + "," + y;
+                return;
+            }
+            x1 = e.X;
+            y1 = pictureBox1.Height - e.Y;
+            g = pictureBox1.CreateGraphics();
+            MouseOverCoordination.Text = x + "," + y;
+            MouseClickedCoordinationX.Text = Convert.ToString(x); MouseClickedCoordinationY.Text = Convert.ToString(y);
+            g.DrawLine(new Pen(Color.Black), x,pictureBox1.Height - y, x1, pictureBox1.Height - y1);
+            x = e.X;y = pictureBox1.Height-e.Y;
+            MouseOverCoordination.Text = x1 + "," + y1;
+            MouseClickedCoordinationX.Text = Convert.ToString(x1);
+            MouseClickedCoordinationY.Text = Convert.ToString(y1);
+        }//划线
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Drawable = true;
+            x = e.X;
+            y = pictureBox1.Height - e.Y;
+            ClearPenwrote();
+        }//鼠标按下动作
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Drawable = false;
+        }//鼠标松开动作
+        private void YCoordination_ValueChanged(object sender, EventArgs e)
+        {
+            y1 = YCoordination.Value;
+            MouseClickedCoordinationY.Text = Convert.ToString(y1);
+            MouseOverCoordination.Text = x + "," + y;
+            
+            DrawPoint();
+        }//Y坐标轴value改变
+        private void XCoordination_ValueChanged(object sender, EventArgs e)
+        {
+            x1 = XCoordination.Value;
+            MouseClickedCoordinationX.Text = Convert.ToString(x1);
+            MouseOverCoordination.Text = x + "," + y;
+
+            DrawPoint();
+        }//X坐标轴value改变
+        private void ClearPenwrote()
+        {
+            if (IfClear.Checked)
+            {
+                try
+                {
+                    pictureBox1.BackgroundImage = Image.FromFile(@openFileDialog1.FileName);
+                }
+                catch
+                {
+                    try
+                    {
+                        g.Clear(Color.White);
+                        return;
+                    }
+                    catch { return; }
+                }
+            }
+        }//擦除画笔
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            openFileDialog1.InitialDirectory = dir;
+            openFileDialog1.ShowDialog();
+        }//选取文件
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Image SelectedPic = Image.FromFile(@openFileDialog1.FileName);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Width = SelectedPic.Width;
+            pictureBox1.Height = SelectedPic.Height;
+            pictureBox1.BackgroundImage = SelectedPic;
+        }//选取完毕
+        private void Locate_Click(object sender, EventArgs e)
+        {
+            ClearPenwrote();
+            try
+            {
+                Convert.ToInt32(MouseClickedCoordinationX.Text);
+                Convert.ToInt32(MouseClickedCoordinationY.Text);
+            }
+            catch
+            {
+                MouseClickedCoordinationX.Text = string.Empty;
+                MouseClickedCoordinationY.Text = string.Empty;
+                return;
+            }
+            if (MouseClickedCoordinationX.Text == string.Empty || MouseClickedCoordinationY.Text == string.Empty)
+                return;
+            y = Convert.ToInt32(MouseClickedCoordinationY.Text);
+            x = Convert.ToInt32(MouseClickedCoordinationX.Text);
+            MouseClickedCoordinationY.Text = Convert.ToString(y);
+            MouseClickedCoordinationX.Text = Convert.ToString(x);
+            DrawPoint();
+        }//手动输入坐标描点
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseClickedCoordinationX.Text = Convert.ToString(x);
+            MouseClickedCoordinationY.Text = Convert.ToString(y);
+            //textBox显示鼠标点的坐标
+            x1 = x;y1 = y;
+            XCoordination.Value = x1;
+            YCoordination.Value = y1;//坐标轴改变value
+            DrawPoint();//描点     
+        }//图上描点
+        private void DrawPoint()
+        {
+            g = pictureBox1.CreateGraphics();
+            g.FillEllipse(Brushes.Black, x1, pictureBox1.Height - y1, 2, 2);
+        }//描点函数
+    }
+}
